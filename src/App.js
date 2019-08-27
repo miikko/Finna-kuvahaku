33e49391a089400f853296d6e0d868f4b8fb3a76
miikko <miikka.oksanen@pp1.inet.fi>
2019-08-27 19:38:15 +0300

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import axios from 'axios'
+const baseUrl = "https://api.finna.fi"
 
-function App() {
+const App = (props) => {
+  const [text, setText] = useState('')
+  const [images, setImages] = useState([])
+  const [resultsFound, setResultsFound] = useState(true)
+
+  const fetchPics = async (event) => {
+    event.preventDefault()
+    const res = await axios.get(`${baseUrl}/v1/search?lookfor=${text}&filter[]=online_boolean:"1"&filter[]=format:"0/Image/"&field[]=title&field[]=images`)
+    if (res.data.resultCount === 0) {
+      setResultsFound(false)
+    } else {
+      setImages(res.data.records.map(record => ({
+        title: record.title,
+        src: `${baseUrl}${record.images[0]}`
+      })))
+    }
+  }
+
+  const handleChange = (event) => {
+    setText(event.target.value)
+    setResultsFound(true)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Finnan kuvahaku</h1>
+      <form onSubmit={fetchPics}>
+        <input type='text' value={text} onChange={handleChange} />
+        <button type='submit'>Hae kuvia</button>
+      </form>
+      {resultsFound
+        ? <div>
+            {images.map(img => 
+              <div key={img.src}>
+                <h2>{img.title}</h2>
+                <img src={img.src} alt="result" width="300" height="300" />
+              </div>
+            )}
+          </div>
+        : <h3 style={{ color: "red" }}>Ei osumia haulle...</h3>
+      }
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
